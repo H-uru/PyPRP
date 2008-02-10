c47f97e2a724fd6bd6e1a59ddc62ff56463a9310
Brendan Dorr <brendandorr@gmail.com>
2008-02-10 21:58:31 +0000

    changed re-added svconvex as softvolume "type" argument

--- src/alcresmanager.py
+++ src/alcresmanager.py
@@ -335,7 +335,7 @@ class alcUruPage:
 
 
                 # Only if this object has the correct alcty, will we pocess it as a softvolume...
-                if alctype == "softvolume":  #Soft Volume
+                if (alctype == "softvolume") or (alctype == "svconvex"):  #Soft Volume
 
                     ## This should be put in plSoftVolumeComplex.Export(page,obj,scnobj,name,SceneNodeRef,softvolumeparser):
                     ## But I'm not touching the softvolume code yet 
